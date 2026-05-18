9892fd0de8215b6132fcc53836f56307c29eebb0
John Dio Lumacang <tcshs.lumacang.johndio.a@gmail.com>
2026-05-18 09:17:16 +0800

    feat: implement audio manager with basic music and sound effect functions

---
+++ autoloads/audio_manager.gd
@@ -0,0 +1,13 @@
+extends Node
+
+func play_music(_stream: AudioStream) -> void:
+	pass
+
+func stop_music() -> void:
+	pass
+
+func play_sfx(_stream: AudioStream, _volume_db: float = 0.0) -> void:
+	pass
+
+func crossfade_to(_stream: AudioStream, _duration: float = 1.0) -> void:
+	pass
